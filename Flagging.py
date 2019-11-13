94bcef4064d96d64cc81f6d18d2cc48e97e75c27
jarr1357 <jarr1357@gmail.com>
2019-11-13 12:01:18 -0500

    Updated Flagging
    Updated Flagging script and removed useless scripts

---
+++ Flagging.py
@@ -0,0 +1,38 @@
+import sys  
+import clr
+import pandas as pd
+
+#Connecting to ProcessBook using .NET
+
+sys.path.append(r'C:\Program Files (x86)\PIPC\AF\PublicAssemblies\4.0')    
+clr.AddReference('OSIsoft.AFSDK')  
+  
+from OSIsoft.AF import *  
+from OSIsoft.AF.PI import *  
+from OSIsoft.AF.Asset import *  
+from OSIsoft.AF.Data import *  
+from OSIsoft.AF.Time import *  
+from OSIsoft.AF.UnitsOfMeasure import *
+
+#Connecting to PiServer
+
+piServers = PIServers()    
+piServer = piServers.DefaultPIServer;
+
+#Loading the Sensor List excel sheet
+
+df = pd.read_excel (r'sensor_list.xlsx')
+sensorlist = pd.DataFrame(df, columns= ['Name'])
+
+scount = 3 #sensor count starts at row 3
+slen = len(sensorlist.index)-1
+
+while scount <= slen:
+  sensor = sensorlist.at[scount, 'Name']
+  scount = scount + 1
+  
+  pt = PIPoint.FindPIPoint(piServer, sensor)  
+  name = pt.Name.lower()
+  current_value = pt.CurrentValue()
+
+  print ('\n Current value of {0}: {1}'.format(sensor, current_value))
